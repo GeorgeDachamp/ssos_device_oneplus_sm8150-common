AOSP-Krypton/device_oneplus_sm8150-common

DeviceSettings: rework touchscreen gesture handling
* merged TouchKeyHandler with existing KeyHandler
* cleaned up imports
* cleaned up corresponding activity and separated the fragment to a class
* migrated activity and fragment to androidx
* removed unnecessary methods and a lot of retarded code (smh) in KeyHandler
* changed the way we handle key events:
    * user choices for events are stored as a system setting
    * gestures are enabled on boot as per uses choices
    * event scanCode and setting key is mapped when KeyHandler is instantiated
    * upon receiving KeyEvent, action is retrieved using the setting key
* use @Keep annotation to retain classes instead of proguard (this is better for small apps)
* cleaned up resources, removed unused strings, relocated theme resource to themes.xml (alertdialog theme is copied from aosp settings app)
* added action to wake up the device (use the same call to wake up device for actions with activity launches)

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

DeviceSettings/src/com/krypton/settings/device/BootCompletedReceiver.java
@@ -1,65 +1,47 @@
 /*
-* Copyright (C) 2013 The OmniROM Project
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ * Copyright (C) 2013 The OmniROM Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.krypton.settings.device;
 
+import static com.android.internal.krypton.hardware.KryptonHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
+
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.provider.Settings;
-import android.text.TextUtils;
 
-import androidx.preference.PreferenceManager;
+import androidx.annotation.Keep;
 
+import com.android.internal.krypton.hardware.KryptonHardwareManager;
+import com.android.internal.krypton.hardware.TouchscreenGesture;
+
+@Keep
 public class BootCompletedReceiver extends BroadcastReceiver {
 
-    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
-
-    private void restore(String file, boolean enabled) {
-        if (file == null) {
-            return;
-        }
-        if (enabled) {
-            Utils.write(file, "1");
-        }
-    }
-
-    private void restore(String file, String value) {
-        if (file == null) {
-            return;
-        }
-        Utils.write(file, value);
-    }
-
     @Override
-    public void onReceive(final Context context, final Intent bootintent) {
-        boolean enabled = false;
-        TouchscreenGestureSettingsActivity.MainSettingsFragment.restoreTouchscreenGestureStates(context);
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-    }
-
-    private boolean hasRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
-    }
-
-    private void setRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
+    public void onReceive(Context context, Intent intent) {
+        final ContentResolver resolver = context.getContentResolver();
+        KryptonHardwareManager mHardwareManager = KryptonHardwareManager.getInstance(context);
+        if (mHardwareManager.isSupported(FEATURE_TOUCHSCREEN_GESTURES)) {
+            for (TouchscreenGesture gesture: mHardwareManager.getTouchscreenGestures()) {
+                int action = Settings.System.getInt(resolver, Utils.getResName(gesture.name), 0);
+                mHardwareManager.setTouchscreenGestureEnabled(gesture, action > 0);
+            }
+        }
     }
 }

DeviceSettings/src/com/krypton/settings/device/fragments/GestureSettingsFragment.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *               2017 The LineageOS Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.settings.device.fragments;
+
+import static android.provider.Settings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK;
+import static com.android.internal.krypton.hardware.KryptonHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.krypton.hardware.KryptonHardwareManager;
+import com.android.internal.krypton.hardware.TouchscreenGesture;
+
+import com.krypton.settings.device.R;
+import com.krypton.settings.device.Utils;
+
+public class GestureSettingsFragment extends PreferenceFragmentCompat {
+
+    private Context mContext;
+    private ContentResolver mResolver;
+    private KryptonHardwareManager mHardwareManager;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+        mResolver = mContext.getContentResolver();
+        mHardwareManager = KryptonHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.touchscreen_gesture_settings, rootKey);
+        final PreferenceScreen screen = getPreferenceScreen();
+        Preference hapticSwitch = screen.findPreference(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
+        if (hapticSwitch != null) {
+            hapticSwitch.setOnPreferenceChangeListener((preference, newValue) ->
+                Settings.System.putInt(mResolver, TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, (Boolean) newValue ? 1 : 0));
+        }
+        if (mHardwareManager.isSupported(FEATURE_TOUCHSCREEN_GESTURES)) {
+            for (TouchscreenGesture gesture: mHardwareManager.getTouchscreenGestures()) {
+                screen.addPreference(new TouchscreenGesturePreference(gesture));
+            }
+        }
+    }
+
+    private final class TouchscreenGesturePreference extends ListPreference {
+
+        @NonNull
+        private final TouchscreenGesture mGesture;
+        private final String key;
+
+        TouchscreenGesturePreference(TouchscreenGesture gesture) {
+            super(mContext);
+            mGesture = gesture;
+            key = Utils.getResName(mGesture.name);
+            int action = Settings.System.getInt(mResolver, key, 0);
+            setKey(key);
+            setEntries(R.array.touchscreen_gesture_action_entries);
+            setEntryValues(R.array.touchscreen_gesture_action_values);
+            setDefaultValue(String.valueOf(action));
+            setSummary("%s");
+            setDialogTitle(R.string.touchscreen_gesture_action_dialog_title);
+            Resources res = mContext.getResources();
+            int resId = res.getIdentifier(key, "string", mContext.getPackageName());
+            setTitle(resId <= 0 ? mGesture.name : res.getString(resId));
+        }
+
+        @Override
+        public boolean callChangeListener(Object newValue) {
+            int action = Integer.parseInt((String) newValue);
+            if (!mHardwareManager.setTouchscreenGestureEnabled(mGesture, action > 0)) {
+                return false;
+            }
+            return super.callChangeListener(newValue);
+        }
+
+        @Override
+        protected boolean persistString(String value) {
+            if (!super.persistString(value)) {
+                return false;
+            }
+            Settings.System.putInt(mResolver, key, Integer.parseInt(value));
+            return true;
+        }
+    }
+}

GeorgeDachamp/ssos_device_oneplus_sm8150-common

DeviceSettings/src/com/ssos/settings/device/BootCompletedReceiver.java
@@ -1,65 +1,47 @@
 /*
-* Copyright (C) 2013 The OmniROM Project
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ * Copyright (C) 2013 The OmniROM Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ssos.settings.device;
 
+import static com.android.internal.lineage.hardware.LineageHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
+
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.provider.Settings;
-import android.text.TextUtils;
 
-import androidx.preference.PreferenceManager;
+import androidx.annotation.Keep;
 
+import com.android.internal.lineage.hardware.LineageHardwareManager;
+import com.android.internal.lineage.hardware.TouchscreenGesture;
+
+@Keep
 public class BootCompletedReceiver extends BroadcastReceiver {
 
-    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
-
-    private void restore(String file, boolean enabled) {
-        if (file == null) {
-            return;
-        }
-        if (enabled) {
-            Utils.write(file, "1");
-        }
-    }
-
-    private void restore(String file, String value) {
-        if (file == null) {
-            return;
-        }
-        Utils.write(file, value);
-    }
-
     @Override
-    public void onReceive(final Context context, final Intent bootintent) {
-        boolean enabled = false;
-        TouchscreenGestureSettingsActivity.MainSettingsFragment.restoreTouchscreenGestureStates(context);
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-    }
-
-    private boolean hasRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
-    }
-
-    private void setRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
+    public void onReceive(Context context, Intent intent) {
+        final ContentResolver resolver = context.getContentResolver();
+        LineageHardwareManager mHardwareManager = LineageHardwareManager.getInstance(context);
+        if (mHardwareManager.isSupported(FEATURE_TOUCHSCREEN_GESTURES)) {
+            for (TouchscreenGesture gesture: mHardwareManager.getTouchscreenGestures()) {
+                int action = Settings.System.getInt(resolver, Utils.getResName(gesture.name), 0);
+                mHardwareManager.setTouchscreenGestureEnabled(gesture, action > 0);
+            }
+        }
     }
 }

DeviceSettings/src/com/ssos/settings/device/fragments/GestureSettingsFragment.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *               2017 The LineageOS Project
+ *               2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ssos.settings.device.fragments;
+
+import static android.provider.Settings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK;
+import static com.android.internal.lineage.hardware.LineageHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.lineage.hardware.LineageHardwareManager;
+import com.android.internal.lineage.hardware.TouchscreenGesture;
+
+import com.ssos.settings.device.R;
+import com.ssos.settings.device.Utils;
+
+public class GestureSettingsFragment extends PreferenceFragmentCompat {
+
+    private Context mContext;
+    private ContentResolver mResolver;
+    private LineageHardwareManager mHardwareManager;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+        mResolver = mContext.getContentResolver();
+        mHardwareManager = LineageHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.touchscreen_gesture_settings, rootKey);
+        final PreferenceScreen screen = getPreferenceScreen();
+        Preference hapticSwitch = screen.findPreference(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
+        if (hapticSwitch != null) {
+            hapticSwitch.setOnPreferenceChangeListener((preference, newValue) ->
+                Settings.System.putInt(mResolver, TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, (Boolean) newValue ? 1 : 0));
+        }
+        if (mHardwareManager.isSupported(FEATURE_TOUCHSCREEN_GESTURES)) {
+            for (TouchscreenGesture gesture: mHardwareManager.getTouchscreenGestures()) {
+                screen.addPreference(new TouchscreenGesturePreference(gesture));
+            }
+        }
+    }
+
+    private final class TouchscreenGesturePreference extends ListPreference {
+
+        @NonNull
+        private final TouchscreenGesture mGesture;
+        private final String key;
+
+        TouchscreenGesturePreference(TouchscreenGesture gesture) {
+            super(mContext);
+            mGesture = gesture;
+            key = Utils.getResName(mGesture.name);
+            int action = Settings.System.getInt(mResolver, key, 0);
+            setKey(key);
+            setEntries(R.array.touchscreen_gesture_action_entries);
+            setEntryValues(R.array.touchscreen_gesture_action_values);
+            setDefaultValue(String.valueOf(action));
+            setSummary("%s");
+            setDialogTitle(R.string.touchscreen_gesture_action_dialog_title);
+            Resources res = mContext.getResources();
+            int resId = res.getIdentifier(key, "string", mContext.getPackageName());
+            setTitle(resId <= 0 ? mGesture.name : res.getString(resId));
+        }
+
+        @Override
+        public boolean callChangeListener(Object newValue) {
+            int action = Integer.parseInt((String) newValue);
+            if (!mHardwareManager.setTouchscreenGestureEnabled(mGesture, action > 0)) {
+                return false;
+            }
+            return super.callChangeListener(newValue);
+        }
+
+        @Override
+        protected boolean persistString(String value) {
+            if (!super.persistString(value)) {
+                return false;
+            }
+            Settings.System.putInt(mResolver, key, Integer.parseInt(value));
+            return true;
+        }
+    }
+}