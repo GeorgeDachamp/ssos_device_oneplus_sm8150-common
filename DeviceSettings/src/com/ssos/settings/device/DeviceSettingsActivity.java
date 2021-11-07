AOSP-Krypton/device_oneplus_sm8150-common

sm8150-common: introduce DeviceSettings

DeviceSettings/src/com/krypton/settings/device/DeviceSettingsActivity.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.settings.device;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.krypton.settings.device.fragments.DeviceSettingsFragment;
+
+public class DeviceSettingsActivity extends FragmentActivity {
+
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getResources().getString(R.string.device_settings_title));
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(android.R.id.content, new DeviceSettingsFragment(), null)
+                .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceSettings/src/com/krypton/settings/device/KeyHandler.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.settings.device;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+
+import com.android.internal.os.DeviceKeyHandler;
+
+public class KeyHandler implements DeviceKeyHandler {
+
+    // Slider key codes
+    private static final int MODE_NORMAL = 601;
+    private static final int MODE_VIBRATION = 602;
+    private static final int MODE_SILENCE = 603;
+
+    // Vibration effects
+    private static final VibrationEffect MODE_NORMAL_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
+    private static final VibrationEffect MODE_VIBRATION_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
+
+    private final Context mContext;
+    private final AudioManager mAudioManager;
+    private final Vibrator mVibrator;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+
+        mAudioManager = mContext.getSystemService(AudioManager.class);
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public KeyEvent handleKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_DOWN) {
+            return event;
+        }
+
+        int scanCode = event.getScanCode();
+
+        switch (scanCode) {
+            case MODE_NORMAL:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                doHapticFeedback(MODE_NORMAL_EFFECT);
+                break;
+            case MODE_VIBRATION:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                break;
+            case MODE_SILENCE:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                break;
+            default:
+                return event;
+        }
+        sendSliderBroadcast(scanCode);
+        return null;
+    }
+
+    private void sendSliderBroadcast(int code) {
+        Intent intent = new Intent("KeyEvent.SLIDER_KEY_CHANGED");
+        intent.putExtra("SLIDER_POSITION", code - MODE_NORMAL);
+        mContext.sendBroadcast(intent);
+    }
+
+    private void doHapticFeedback(VibrationEffect effect) {
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(effect);
+        }
+    }
+}

DeviceSettings/src/com/krypton/settings/device/Utils.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.settings.device;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+public class Utils {
+
+    public static boolean isChecked(Preference preference) {
+        boolean checked = false;
+        if (preference instanceof SwitchPreference) {
+            checked = ((SwitchPreference) preference).isChecked();
+        } else if (preference instanceof SwitchPreference) {
+            checked = ((CheckBoxPreference) preference).isChecked();
+        }
+        return checked;
+    }
+
+}

GeorgeDachamp/ssos_device_oneplus_sm8150-common

DeviceSettings/src/com/ssos/settings/device/DeviceSettingsActivity.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ssos.settings.device;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.ssos.settings.device.fragments.DeviceSettingsFragment;
+
+public class DeviceSettingsActivity extends FragmentActivity {
+
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getResources().getString(R.string.device_settings_title));
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(android.R.id.content, new DeviceSettingsFragment(), null)
+                .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceSettings/src/com/ssos/settings/device/KeyHandler.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ssos.settings.device;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+
+import com.android.internal.os.DeviceKeyHandler;
+
+public class KeyHandler implements DeviceKeyHandler {
+
+    // Slider key codes
+    private static final int MODE_NORMAL = 601;
+    private static final int MODE_VIBRATION = 602;
+    private static final int MODE_SILENCE = 603;
+
+    // Vibration effects
+    private static final VibrationEffect MODE_NORMAL_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
+    private static final VibrationEffect MODE_VIBRATION_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
+
+    private final Context mContext;
+    private final AudioManager mAudioManager;
+    private final Vibrator mVibrator;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+
+        mAudioManager = mContext.getSystemService(AudioManager.class);
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public KeyEvent handleKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_DOWN) {
+            return event;
+        }
+
+        int scanCode = event.getScanCode();
+
+        switch (scanCode) {
+            case MODE_NORMAL:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                doHapticFeedback(MODE_NORMAL_EFFECT);
+                break;
+            case MODE_VIBRATION:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                break;
+            case MODE_SILENCE:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                break;
+            default:
+                return event;
+        }
+        sendSliderBroadcast(scanCode);
+        return null;
+    }
+
+    private void sendSliderBroadcast(int code) {
+        Intent intent = new Intent("KeyEvent.SLIDER_KEY_CHANGED");
+        intent.putExtra("SLIDER_POSITION", code - MODE_NORMAL);
+        mContext.sendBroadcast(intent);
+    }
+
+    private void doHapticFeedback(VibrationEffect effect) {
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(effect);
+        }
+    }
+}

DeviceSettings/src/com/ssos/settings/device/Utils.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ssos.settings.device;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+public class Utils {
+
+    public static boolean isChecked(Preference preference) {
+        boolean checked = false;
+        if (preference instanceof SwitchPreference) {
+            checked = ((SwitchPreference) preference).isChecked();
+        } else if (preference instanceof SwitchPreference) {
+            checked = ((CheckBoxPreference) preference).isChecked();
+        }
+        return checked;
+    }
+
+}