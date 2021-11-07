AOSP-Krypton/device_oneplus_sm8150-common

DeviceSettings: allow customising alert slider actions
* partially based on https://github.com/yaap/device_oneplus_sm8150-common/commit/aa5a4113c1a226b00513644be39e1235dbfd3b87#diff-31fbb32cd36fd2d65a5924f5fc7ced28b57842ac983aad80e1b75dfbb7bd8fee
* cleaned up some code meanwhile

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

DeviceSettings/src/com/krypton/settings/device/fragments/DeviceSettingsFragment.java
@@ -16,45 +16,89 @@
 
 package com.krypton.settings.device.fragments;
 
-import static android.provider.Settings.System.PEAK_REFRESH_RATE;
-import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_BOTTOM;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_MIDDLE;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_TOP;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
-import com.krypton.settings.device.R;
-import com.krypton.settings.device.Utils;
+import com.android.internal.R;
 
-public class DeviceSettingsFragment extends PreferenceFragmentCompat {
+public class DeviceSettingsFragment extends PreferenceFragmentCompat
+        implements OnPreferenceChangeListener {
     private static final String KEY_FORCE_90 = "force_90_preference";
+    private static final String KEY_ALERT_SLIDER_BOTTOM = "alert_slider_bottom_preference";
+    private static final String KEY_ALERT_SLIDER_MIDDLE = "alert_slider_middle_preference";
+    private static final String KEY_ALERT_SLIDER_TOP = "alert_slider_top_preference";
     private Context mContext;
 
     @Override
     public void onCreatePreferences(Bundle bundle, String key) {
-        setPreferencesFromResource(R.xml.device_settings, key);
+        setPreferencesFromResource(com.krypton.settings.device.R.xml.device_settings, key);
         mContext = getContext();
+        findPreference(KEY_FORCE_90).setOnPreferenceChangeListener(this);
+
+        final String[] entryValues = new String[] {
+            mContext.getString(R.string.alert_slider_mode_normal),
+            mContext.getString(R.string.alert_slider_mode_priority),
+            mContext.getString(R.string.alert_slider_mode_vibrate),
+            mContext.getString(R.string.alert_slider_mode_silent),
+            mContext.getString(R.string.alert_slider_mode_dnd)
+        };
+        final ContentResolver contentResolver = mContext.getContentResolver();
+
+        ListPreference alertSliderBottomPref = findPreference(KEY_ALERT_SLIDER_BOTTOM);
+        alertSliderBottomPref.setEntryValues(entryValues);
+        String value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_BOTTOM);
+        alertSliderBottomPref.setValue(value != null ? value : entryValues[0]);
+        alertSliderBottomPref.setOnPreferenceChangeListener(this);
+
+        ListPreference alertSliderMiddlePref = findPreference(KEY_ALERT_SLIDER_MIDDLE);
+        alertSliderMiddlePref.setEntryValues(entryValues);
+        value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_MIDDLE);
+        alertSliderMiddlePref.setValue(value != null ? value : entryValues[2]);
+        alertSliderMiddlePref.setOnPreferenceChangeListener(this);
+
+        ListPreference alertSliderTopPref = findPreference(KEY_ALERT_SLIDER_TOP);
+        alertSliderTopPref.setEntryValues(entryValues);
+        value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_TOP);
+        alertSliderTopPref.setValue(value != null ? value : entryValues[3]);
+        alertSliderTopPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        String key = preference.getKey();
-        if (key != null) {
-            if (key.equals(KEY_FORCE_90)) {
-                boolean state = Utils.isChecked(preference);
-                int rate = state == true ? 90 : 60;
-                setRefreshRate(rate);
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case KEY_FORCE_90:
+                setRefreshRate(((Boolean) newValue) ? 90 : 60);
                 return true;
-            }
+            case KEY_ALERT_SLIDER_BOTTOM:
+                return putString(ALERTSLIDER_MODE_POSITION_BOTTOM, newValue);
+            case KEY_ALERT_SLIDER_MIDDLE:
+                return putString(ALERTSLIDER_MODE_POSITION_MIDDLE, newValue);
+            case KEY_ALERT_SLIDER_TOP:
+                return putString(ALERTSLIDER_MODE_POSITION_TOP, newValue);
+            default:
+                return false;
         }
-        return super.onPreferenceTreeClick(preference);
     }
 
     private void setRefreshRate(int rate) {
-        Settings.System.putInt(mContext.getContentResolver(), PEAK_REFRESH_RATE, rate);
-        Settings.System.putInt(mContext.getContentResolver(), MIN_REFRESH_RATE, rate);
+        final ContentResolver contentResolver = mContext.getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.PEAK_REFRESH_RATE, rate);
+        Settings.System.putInt(contentResolver, Settings.System.MIN_REFRESH_RATE, rate);
+    }
+
+    private boolean putString(String key, Object value) {
+        return Settings.System.putString(mContext.getContentResolver(), key, (String) value);
     }
 }

GeorgeDachamp/ssos_device_oneplus_sm8150-common

DeviceSettings/src/com/ssos/settings/device/fragments/DeviceSettingsFragment.java
@@ -16,45 +16,89 @@
 
 package com.ssos.settings.device.fragments;
 
-import static android.provider.Settings.System.PEAK_REFRESH_RATE;
-import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_BOTTOM;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_MIDDLE;
+import static android.provider.Settings.System.ALERTSLIDER_MODE_POSITION_TOP;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
-import com.ssos.settings.device.R;
-import com.ssos.settings.device.Utils;
+import com.android.internal.R;
 
-public class DeviceSettingsFragment extends PreferenceFragmentCompat {
+public class DeviceSettingsFragment extends PreferenceFragmentCompat
+        implements OnPreferenceChangeListener {
     private static final String KEY_FORCE_90 = "force_90_preference";
+    private static final String KEY_ALERT_SLIDER_BOTTOM = "alert_slider_bottom_preference";
+    private static final String KEY_ALERT_SLIDER_MIDDLE = "alert_slider_middle_preference";
+    private static final String KEY_ALERT_SLIDER_TOP = "alert_slider_top_preference";
     private Context mContext;
 
     @Override
     public void onCreatePreferences(Bundle bundle, String key) {
-        setPreferencesFromResource(R.xml.device_settings, key);
+        setPreferencesFromResource(com.ssos.settings.device.R.xml.device_settings, key);
         mContext = getContext();
+        findPreference(KEY_FORCE_90).setOnPreferenceChangeListener(this);
+
+        final String[] entryValues = new String[] {
+            mContext.getString(R.string.alert_slider_mode_normal),
+            mContext.getString(R.string.alert_slider_mode_priority),
+            mContext.getString(R.string.alert_slider_mode_vibrate),
+            mContext.getString(R.string.alert_slider_mode_silent),
+            mContext.getString(R.string.alert_slider_mode_dnd)
+        };
+        final ContentResolver contentResolver = mContext.getContentResolver();
+
+        ListPreference alertSliderBottomPref = findPreference(KEY_ALERT_SLIDER_BOTTOM);
+        alertSliderBottomPref.setEntryValues(entryValues);
+        String value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_BOTTOM);
+        alertSliderBottomPref.setValue(value != null ? value : entryValues[0]);
+        alertSliderBottomPref.setOnPreferenceChangeListener(this);
+
+        ListPreference alertSliderMiddlePref = findPreference(KEY_ALERT_SLIDER_MIDDLE);
+        alertSliderMiddlePref.setEntryValues(entryValues);
+        value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_MIDDLE);
+        alertSliderMiddlePref.setValue(value != null ? value : entryValues[2]);
+        alertSliderMiddlePref.setOnPreferenceChangeListener(this);
+
+        ListPreference alertSliderTopPref = findPreference(KEY_ALERT_SLIDER_TOP);
+        alertSliderTopPref.setEntryValues(entryValues);
+        value = Settings.System.getString(contentResolver, ALERTSLIDER_MODE_POSITION_TOP);
+        alertSliderTopPref.setValue(value != null ? value : entryValues[3]);
+        alertSliderTopPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        String key = preference.getKey();
-        if (key != null) {
-            if (key.equals(KEY_FORCE_90)) {
-                boolean state = Utils.isChecked(preference);
-                int rate = state == true ? 90 : 60;
-                setRefreshRate(rate);
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case KEY_FORCE_90:
+                setRefreshRate(((Boolean) newValue) ? 90 : 60);
                 return true;
-            }
+            case KEY_ALERT_SLIDER_BOTTOM:
+                return putString(ALERTSLIDER_MODE_POSITION_BOTTOM, newValue);
+            case KEY_ALERT_SLIDER_MIDDLE:
+                return putString(ALERTSLIDER_MODE_POSITION_MIDDLE, newValue);
+            case KEY_ALERT_SLIDER_TOP:
+                return putString(ALERTSLIDER_MODE_POSITION_TOP, newValue);
+            default:
+                return false;
         }
-        return super.onPreferenceTreeClick(preference);
     }
 
     private void setRefreshRate(int rate) {
-        Settings.System.putInt(mContext.getContentResolver(), PEAK_REFRESH_RATE, rate);
-        Settings.System.putInt(mContext.getContentResolver(), MIN_REFRESH_RATE, rate);
+        final ContentResolver contentResolver = mContext.getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.PEAK_REFRESH_RATE, rate);
+        Settings.System.putInt(contentResolver, Settings.System.MIN_REFRESH_RATE, rate);
+    }
+
+    private boolean putString(String key, Object value) {
+        return Settings.System.putString(mContext.getContentResolver(), key, (String) value);
     }
 }